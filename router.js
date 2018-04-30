cea6b96e1ae587c3f2d836305264fa298e409d9b
Zhang <zhangwentaosnake@163.com>
2018-04-30 18:36:18 +0800

    beta
    alpha to beta

--- router.js
+++ router.js
@@ -8,61 +8,57 @@ const router = new Router()
 // 记录全部请求
 router.all('/*', async function(ctx, next){
     await next()
-    console.log(ctx.method+" - " + ctx.path +"   [" + ctx.status + "]   " + JSON.stringify(ctx.body))
+    console.log(new Date() + ' :' +ctx.method+" - " + ctx.path +"   [" + ctx.status + "]   " + JSON.stringify(ctx.body))
 })
 
 // 根据openId获取通讯录列表
 router.get('/address/tables', async function(ctx, next){
-    await db.getTables(ctx.request.body).then(function(rows){ctx.response.body=rows}).catch(function(err){console.log(err)})
+    await db.getTables(ctx.request.query).then(function(rows){
+        console.log(rows)
+        ctx.response.body=rows
+    }).catch(function(err){console.log(err)})
     next();
 })
 
 // 根据openId插入可获得通讯录信息
 router.post('/address/tables', async function(ctx, next){
-    await db.insertTables(ctx.request.body).then(async function(body, next){
-        // 重新获取可获得的通讯录列表并返回
-        await db.getTables(body).then(function(rows){ctx.response.body=rows}).catch(function(err){console.log(err)})
-        next()
-        // 创建同名的通讯录表结构
-        await db.createTable(body).then(function(rows){console.log(rows)}).catch(function(err){console.log(err)})
-        next()
+    await db.insertTables(ctx.request.body).then(function(rows){
+        ctx.response.body=rows
     }).catch(function(err){console.log(err)})
     next()
 })
 
 // 获取选定的通讯录详细信息
 router.get('/address/list', async function(ctx, next){
-    await db.getDetail(ctx.request.body).then(function(rows){ctx.response.body=rows}).catch(function(err){result = err;})
+    await db.getDetail(ctx.request.query).then(function(rows){
+        ctx.response.body=rows
+    }).catch(function(err){console.log(err)})
     next();
 })
 
 // 向选定的通讯录中插入新的详细信息
 router.post('/address/list', async function(ctx, next){
-    await db.insertDetail(ctx.request.body).then(async function(body, next){
-        // 重新获取选定的通讯录中全部的详细信息
-        await db.getDetail(body).then(function(rows){ctx.response.body=rows}).catch(function(err){result = err;})
-        next();
-    }).catch(function(err){result = err;})
+    console.log(ctx.request.body)
+    await db.insertDetail(ctx.request.body).then(function(rows){
+		ctx.response.body=rows
+    }).catch(function(err){console.log(err)})
     next();
 })
 
 // 修改选定的通讯录中的详细信息
 router.put('/address/list', async function(ctx, next){
-    await db.updateDetail(ctx.recent.body).then(async function(body, next){
-        // 重新获取选定的通讯录中全部的详细信息
-        await db.getDetail(body).then(function(rows){ctx.response.body=rows}).catch(function(err){result = err;})
-        next();
-    }).catch(function(err){result = err;})
+    await db.updateDetail(ctx.request.body).then(function(rows){
+        ctx.response.body=rows
+    }).catch(function(err){console.log(err)})
     next();
 })
 
 // 根据code获取openid
 router.get('/wx/onLogin', async function(ctx, next){
-    let code = ctx.request.body.code;
-    let result = null;
+    let code = ctx.request.query.code;
     await wx.getSessionKey(code).then(function(res){
-        result = JSON.parse(res)
-        ctx.response.body = result
+        console.log(res)
+        ctx.response.body = res
     }).catch(function(err){
         console.log(err)
     })
--- sqlite.js
+++ sqlite.js
@@ -1,12 +1,12 @@
 var sqlite3 = require('sqlite3').verbose();
 
 // 创建或打开数据库
-var db = new sqlite3.Database('address-book.db',sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE)
+var db = new sqlite3.Database('addressbook.db',sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE)
 
 // 创建表结构
-db.run('CREATE TABLE tablelist(primary INTEGER PRIMARY KEY AUTOINCREMENT, openid TEXT, tablename TEXT);', {},
+db.run('CREATE TABLE IF NOT EXISTS tablelist (xid INTEGER PRIMARY KEY AUTOINCREMENT, openid TEXT, tablename TEXT);', [],
     function(err){
-        if(!err){
+        if(err){
             console.log(err);
         }
     });
@@ -14,7 +14,7 @@ db.run('CREATE TABLE tablelist(primary INTEGER PRIMARY KEY AUTOINCREMENT, openid
 // 根据openId获取通讯录列表
 exports.getTables = function(body){
     return new Promise(function(resolve,reject){
-        db.run('SELECT primary,tableName FROM tablelist WHERE openid=$openid;', body, function(err,rows){
+        db.all('SELECT xid,tablename FROM tablelist WHERE openid=' + '"' + body.openid + '"', function(err,rows){
             if(!err){
                 resolve(rows)
             }else{
@@ -27,36 +27,41 @@ exports.getTables = function(body){
 // 根据openId插入可获得通讯录信息
 exports.insertTables = function(body){
     return new Promise(function(resolve,reject){
-        db.run('INSERT INTO tablelist VALUES (null, $openid, $tablename);', body, function(err,rows){
-            if(!err){
-                console.log(rows)
-                resolve(body)
-            }else{
-                reject(err)
-            }
-        })
-    })
-}
-
-// 创建新的通讯录表结构
-exports.createTable = function(body){
-    return new Promise(function(resolve,reject){
-        db.run('CREATE TABLE $tablename(primary INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, mobile TEXT, city TEXT, status TEXT);', body,
-            function(err, rows){
+        db.serialize(function(){
+            // 将可获得的新表插入tablelist中
+            db.run('INSERT INTO tablelist (openid,tablename) VALUES (' + '"' + body.openid + '","' + body.tablename + '")', function(err){
+                if(!err){
+                    console.log(this)
+                }else{
+                    console.log(err)
+                }
+            })
+            // 根据提交的tablename创建新表
+            db.run('CREATE TABLE IF NOT EXISTS ' + '"' + body.tablename + '"' + ' (xid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, mobile TEXT, city TEXT, status TEXT);', function(err){
+                if(!err){
+					console.log(this)
+                }else{
+                    console.log(err)
+                }
+            })
+            // 重新获取tablelist中的可获得表单
+            db.all('SELECT xid,tablename FROM tablelist WHERE openid=' + '"' + body.openid + '"', function(err,rows){
                 if(!err){
                     resolve(rows)
+                    console.log(rows)
                 }else{
                     reject(err)
+                    console.log(err)
                 }
-            }
-        )
+            })
+        })
     })
 }
 
 // 获取选定的通讯录详细信息
 exports.getDetail = function(body){
     return new Promise(function(resolve,reject){
-        db.run('SELECT primary,name,mobile,city,status FROM $tablename;', body, function(err, rows){
+        db.all('SELECT xid,name,mobile,city,status FROM ' + '"' + body.tablename + '"', function(err, rows){
             if(!err){
                 resolve(rows)
             }else{
@@ -69,13 +74,25 @@ exports.getDetail = function(body){
 // 插入新的通讯录详细信息
 exports.insertDetail = function(body){
     return new Promise(function(resolve,reject){
-        db.run('INSERT INTO $tablename VALUES (null, $name, $mobile, $city, $status)', body, function(err, rows){
-            if(!err){
-                console.log(rows)
-                resolve(body)
-            }else{
-                reject(err)
-            }
+        db.serialize(function(){
+            // 将新的详细信息插入选定表中
+            db.run('INSERT INTO ' + '"' + body[0].tablename + '"' + ' (name, mobile, city, status) VALUES (' + '"' + body[0].name + '","' + body[0].mobile +'","' +  body[0].city +'","' +  body[0].status + '")', function(err){
+                if(!err){
+                    console.log(this)
+				}else{
+                    console.log(err)
+                }
+            })
+            // 重新获取选定表的全部内容
+            db.all('SELECT xid,name,mobile,city,status FROM ' + '"' + body[0].tablename + '"', function(err, rows){
+                if(!err){
+                    console.log(rows)
+                    resolve(rows)
+                }else{
+                    console.log(err)
+                    reject(err)
+                }
+            })
         })
     })
 }
@@ -83,37 +100,25 @@ exports.insertDetail = function(body){
 // 修改选定的通讯录中的详细信息
 exports.updateDetail = function(body){
     return new Promise(function(resolve,reject){
-        db.run('UPDATE $tablename SET $modifykey=$newcontent where primary=$modifyid', body, function(err, rows){
-            if(!err){
-                console.log(rows)
-                resolve(body)
-            }else{
-                reject(err)
-            }
+        db.serialize(function(){
+            // 根据新的详细信息修改选定表
+            db.run('UPDATE ' + '"' + body.tablename + '"' + ' SET ' + body.modifykey + '=' + '"' + body.newcontent + '"' + ' where xid=' + body.modifyid, function(err){
+                if(!err){
+                    console.log(this)
+                }else{
+                    console.log(err)
+                }
+            })
+            // 重新获取选定表的全部内容
+            db.all('SELECT xid,name,mobile,city,status FROM ' + '"' + body.tablename + '"', function(err, rows){
+                if(!err){
+                    console.log(rows)
+                    resolve(rows)
+                }else{
+                    console.log(err)
+                    reject(err)
+                }
+            })
         })
-    })
-}
-
-// 创建新用户
-exports.insertInfo = function(data) {
-    return new Promise(function(resolve, reject){
-        data.$create_time = new Date().getTime();
-        data.$update_time = '';
-        db.run('INSERT INTO content VALUES (null, $truename, $mobile , $city, $recent, $create_time, $update_time, $wx_id, $wx_name);', data, function(err, row){
-            if(!err){
-                resolve(this.lastID);
-            }else{
-                reject(err);
-            }
-        })
-    })
-}
-exports.createOrNothingTODO = function({session_key='',openid=''}){
-    db.run('select * from content where openid = $openid',{$openid:openid},function(err,rows){
-        if(!err){
-            console.log(rows)
-        }else{
-            
-        }
-    })
+	})
 }
