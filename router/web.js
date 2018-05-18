70b0fdf9e290146b24d775a437f0d87205091802
libing <makeup1123@163.com>
2018-05-18 18:12:14 +0800

    设置组件级缓存策略

--- router/web.js
+++ router/web.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 const path = require('path')
+const LRU = require('lru-cache')
 const { createBundleRenderer } = require('vue-server-renderer')
 // SSR基础页面模板
 const template = require('fs').readFileSync(path.resolve(__dirname, '../src/index.server.template.html'), 'utf-8')
@@ -9,10 +10,10 @@ function createRenderer (bundle, options) {
   // https://github.com/vuejs/vue/blob/dev/packages/vue-server-renderer/README.md#why-use-bundlerenderer
   return createBundleRenderer(bundle, Object.assign(options, {
     // for component caching
-    // cache: LRU({
-    //   max: 1000,
-    //   maxAge: 1000 * 60 * 15
-    // }),
+    cache: LRU({
+      max: 1000,
+      maxAge: 1000 * 60 * 15
+    }),
     // this is only needed when vue-server-renderer is npm-linked
     basedir: resolve('../dist'),
     // recommended for performance
